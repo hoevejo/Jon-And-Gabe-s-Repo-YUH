81fa010235118ab62c4de7bb0d2be28b2cc516da
Gabriel J. Armstrong <armstrga@eos20.cis.gvsu.edu>
2021-10-31 21:54:33 -0400

    We tried our best I think

--- note.js
+++ note.js
@@ -1,9 +1,12 @@
 /**
+ * @author Gabe Armstrong
+ * @author Jon Hoeve
  * Functions for the note routes.
  *
  */
 
 // Pull in the mongoose library.
+const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 
 // Grab the Schema object.
@@ -74,35 +77,52 @@ exports.postOne = async function(req, res){
 
 // Create the function for deleting a note.
 // export is so we can use it in app.js.
-exports.removeOne = async function(req, res){
-	Note.findById(id, function(err, doc){
-		if(err){
-			res.sendStatus(400);
-			console.log(err);
-			return;
+exports.delete = async function(req, res){
+	const notes = await Note.find({ 
+		ObjectId: { 
+			$regex: req.params.searchTerm 
 		}
-		doc.remove(callback);
-		
+	});
+
+	if(notes.length == 0){
+		res.sendStatus(404);
+		return;
 	}
 	
-
+	notes.deleteOne();
+	res.sendStatus(200);
+	return;
 }
 
 // Create the function for overwrite a note.
 // export is so we can use it in app.js.
 exports.putOne = async function(req, res){
-	Note.put('/notes/:objectid' , (req, res, next)=>
-	{
-		let newNote = new note({
-			userId: req.params.userId,
-			subject: req.body.subject,
-			course: req.body.course,
-			note: req.body.note
+	let notes = await Note.find({ 
+		ObjectId: { 
+			$regex: req.params.searchTerm 
+		}
+	});
+
+	if(notes.length == 0){
+		res.sendStatus(404);
+		return;
+	}
+	
+	Note.findOne(req.params.ObjectId).then(doc => {
+		item["subject"] = req.body.subject;
+		item["course"] = req.body.course;
+		item["note"] = req.body.note;
+		item["date"] = req.body.date;
+		doc.save();
+	});
 
 }
 
 // Create the function for update a note.
 // export is so we can use it in app.js.
 exports.updateOne = async function(req, res){
-
+	Note.findByIdAndUpdate(ObjectId, {note : req.body.note});
+	Note.findByIdAndUpdate(ObjectId, {subject : req.body.subject});
+	Note.findByIdAndUpdate(ObjectId, {course: req.body.course});
+	Note.findByIdAndUpdate(ObjectId, {date : req.body.date});
 }
